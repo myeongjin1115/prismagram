0017a6b6c0a6c3648997fa89641134c2bceb3b1b
myeongjin1115 <myeongjin1115@gmail.com>
2020-01-21 14:50:15 +0900

    #3.12 me Resolver + Prisma's Limitations

---
+++ src/api/User/me/me.js
@@ -0,0 +1,18 @@
+import { isAuthenticated } from "../../../middlewares";
+import { prisma } from "../../../../generated/prisma-client";
+import { USER_FRAGMENT } from "../../../fragments";
+
+export default {
+  Query: {
+    me: async (_, __, { request }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      const userProfile = prisma.user({ id: user.id });
+      const posts = prisma.user({ id: user.id }).posts();
+      return {
+          user: userProfile,
+          posts
+      }
+    }
+  }
+};
